ccafa9b7da39700d29a14623c32603fdb15632f9
sureshkini09 <01fe21bec255@gmail.com>
2024-10-17 20:14:37 +0530

    Design CLA

---
+++ Day_1_CLA/Day_1_CLA.srcs/sources_1/new/CLA_tb.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.10.2024 20:12:24
+// Design Name: 
+// Module Name: CLA_tb
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module CLA_tb(
+
+    );
+endmodule
---
+++ Day_1_CLA/Day_1_CLA.srcs/sources_1/new/Carry_Look_Ahead_Adder.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 17.10.2024 20:12:24
+// Design Name: 
+// Module Name: Carry_Look_Ahead_Adder
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module Carry_Look_Ahead_Adder(
+
+    );
+endmodule
